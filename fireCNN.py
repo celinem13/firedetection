0bfe0c7d84afe858e27a2f438ecae5a4526bb2a0
Clayton Clark <clayton.clark12@yahoo.com>
2021-12-01 16:24:37 -0800

    Add files via upload

--- fireCNN.py
+++ fireCNN.py
@@ -15,28 +15,6 @@ def unpickle(file):
 
 # Normalize pixel values to be between 0 and 1
 #train_images, test_images = train_images / 255.0, test_images / 255.0
-#print(train_images[0])
-
-#plt.imshow(train_images[0])
-#plt.show()
-
-print(train_labels.shape)
-
-img_path = "../dataset/NA2 patches/NA2 patches/"
-
-img = cv2.imread(img_path +"0.png")
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) 
-h, s, v = cv2.split(hsv)
-
-lim = 255 - 30
-v[v > lim] = 255
-v[v <= lim] += 30
-
-final_hsv = cv2.merge((h, s, v))
-
-cv2.imshow("img",hsv)
-cv2.imshow("img2",final_hsv)
-cv2.waitKey(0)
 
 #train_images = 0
 #train_labels = 0
